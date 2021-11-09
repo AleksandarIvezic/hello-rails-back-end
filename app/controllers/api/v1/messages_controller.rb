1f8b289cdc74de9327a86d6838db84e020a5c6b7
Aleksandar <a.ivezic@yahoo.com>
2021-11-09 10:42:54 +0100

    Set up routes and create set_default_format method

--- app/controllers/api/v1/messages_controller.rb
+++ app/controllers/api/v1/messages_controller.rb
@@ -1,12 +1,17 @@
-class V1::MessagesController < ApplicationController
-  before_action :set_greeting
+class Api::V1::MessagesController < ApplicationController
+  before_action :set_greeting, :set_default_format
 
   def index   
   end
 
+  private 
+
   def set_greeting
-    @posts = Post.all
     @message = Message.find(rand(1..5))
     @greeting = @message.greeting
+  end
+
+  def set_default_format
+    request.format = :json
   end
 end
