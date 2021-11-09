789601b3e51caf1235e1f5987041f0f67e176242
Aleksandar <a.ivezic@yahoo.com>
2021-11-09 10:44:01 +0100

    Fix rubocop errors

--- app/controllers/api/v1/messages_controller.rb
+++ app/controllers/api/v1/messages_controller.rb
@@ -1,10 +1,9 @@
 class Api::V1::MessagesController < ApplicationController
   before_action :set_greeting, :set_default_format
 
-  def index   
-  end
+  def index; end
 
-  private 
+  private
 
   def set_greeting
     @message = Message.find(rand(1..5))
